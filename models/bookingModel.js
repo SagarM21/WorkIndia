46bb502d711c72245c38c4349f5cabfadcfdbf19
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-09-14 11:03:54 +0530

    add: booking model details

---
+++ models/bookingModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const Train = require("./trainModel");
+const User = require("./userModel");
+
+const bookingSchema = new mongoose.Schema(
+  {
+    train: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: Train, // Reference to the Train model
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: User, // Reference to the User model
+      required: true,
+    },
+    booking_id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    seat_numbers: {
+      type: [Number], // Array to store seat numbers
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Booking = mongoose.model("Booking", bookingSchema);
+
+module.exports = Booking;
---
+++ utils/generateBookingId.js
@@ -0,0 +1,5 @@
+const generateBookingID = () => {
+  return Math.random().toString(36).substr(2, 10);
+};
+
+module.exports = generateBookingID;
